122d45437ed8c227dd3e77e6f6f11b0ec823ec6f
Guilherme <Fireguiquenbr@gmail.com>
2023-08-28 09:56:49 -0300

    Update textoEmC.c

--- Week 01 - C/exercícios/textoEmC.c
+++ Week 01 - C/exercícios/textoEmC.c
@@ -2,5 +2,6 @@
 
 int main(void){
     printf("Olá, isso é o início de um texto. Aqui ocorrerá uma quebra de linha. \n");
-    printf("Esta quebra só ocorreu devido ao '\\n'. \n");
+    printf("Esta quebra só ocorreu devido ao '\\n'. ");
+    printf("O printf anterior não possui o \\n, logo, esta frase não será printada em outra linha.");
 }
