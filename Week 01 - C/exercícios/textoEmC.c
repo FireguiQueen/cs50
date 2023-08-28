39ee6ed1d86b917984f4845f183163a7572ffc45
Guilherme <Fireguiquenbr@gmail.com>
2023-08-27 22:55:33 -0300

    Create textoEmC.c

---
+++ Week 01 - C/exercícios/textoEmC.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+
+int main(void){
+    printf("Olá, isso é o início de um texto. Aqui ocorrerá uma quebra de linha. \n");
+    printf("Esta quebra só ocorreu devido ao '\\n'. \n");
+}
