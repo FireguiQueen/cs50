3bbbe74988f90991c2d1d77bdd8d47c52f60b769
Guilherme <Fireguiquenbr@gmail.com>
2023-08-28 18:56:26 -0300

    update

--- Week 01 - C/hello.c
+++ Week 01 - C/hello.c
@@ -1,7 +1,7 @@
-#include <stdio.h> // inclui a biblioteca para printar no console
+#include <stdio.h> // include = incluir. Ou seja, incluir uma biblioteca.
 
 
-int main(void){ // define a função principal (void fz com que não haja args para a função)
+int main(void){ // define a função principal (void faz com que não haja args para a função)
     printf("Olá mundo!\n") // printf printa no console. \n (newline) serve para o próximo print no console seja na proxima linha
     printf("Estou em outra linha.") // printf printa no console. \n (newline) serve para o próximo print no console seja na proxima linha
 }
