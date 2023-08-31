74ad20ef7ac41f1fd9be4fdbfdb998a63d509989
Guilherme <Fireguiquenbr@gmail.com>
2023-08-31 10:51:27 -0300

    Create soma.c

---
+++ Week 01 - C/exercícios/soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(void){
+    int x, y; 
+
+    printf("Olá! Vamos somar números inteiros?")
+    printf("Digite o valor de X: ");
+    scanf("%d", &x);
+
+    printf("Digite o valor de Y: ");
+    scanf("%d", &y);
+
+    printf("A soma entre X e Y é = %d", x + y);
+
+
+
+}
