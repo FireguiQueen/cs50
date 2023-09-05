2f3169344da266b6ceeb4a0b92200a38ab091297
Guilherme <Fireguiquenbr@gmail.com>
2023-09-04 21:06:07 -0300

    update

---
+++ Week 01 - C/exercícios/Mario.js
@@ -0,0 +1,20 @@
+function construirLargura(largura){
+    const blocos = "#".repeat(largura);
+    return blocos;
+}
+
+function returnDots(dots){
+    return ".".repeat(dots);
+}
+
+function construirPiramide(altura){
+    let width = 0;
+    let totalDots = altura-1; 
+    for(let i = 0; i < altura; i++){
+        console.log(returnDots(totalDots) + '#' + construirLargura(width));
+        width++;
+        totalDots--;
+    }
+}
+
+construirPiramide(9);
