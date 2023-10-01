897e8b36a343aac8ccf993ee1716a92d940d994f
Guilherme <Fireguiquenbr@gmail.com>
2023-10-01 14:57:56 -0300

    Update CalcAvarage.c

---
+++ Week 02 - Array/exercises/CalcAvarage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    float totalNumbers;
+    printf("How many numbers will you use? ");
+    scanf("%f", &totalNumbers);
+
+    float numbers[totalNumbers] = {};
+
+    
+}
