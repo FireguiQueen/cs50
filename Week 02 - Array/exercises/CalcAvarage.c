05001d4904fcbacba889f4ae478e91978fea2739
Guilherme <Fireguiquenbr@gmail.com>
2023-10-01 15:18:42 -0300

    Update CalcAvarage.c

--- Week 02 - Array/exercises/CalcAvarage.c
+++ Week 02 - Array/exercises/CalcAvarage.c
@@ -2,11 +2,21 @@
 
 int main(void)
 {   
-    float totalNumbers;
-    printf("How many numbers will you use? ");
-    scanf("%f", &totalNumbers);
+    float totalScore = 0;
+    int totalNumbers = 0;
+    printf("How many numbers will you use? \n");
+    scanf("%i", &totalNumbers);
 
-    float numbers[totalNumbers] = {};
+    for (int i = 0; i < totalNumbers; i++)
+    {
+        float number = 0;
+        printf("Number %i: ", i + 1);
+        scanf("%f", &number);
+        totalScore += number;
+    }
 
-    
+    printf("Total: %f \n", totalScore);
+    printf("Length: %i \n", totalNumbers);
+    printf("Avarage: %f \n", totalScore / totalNumbers);
+
 }
