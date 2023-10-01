4b49c5f25ea3ce3e9a5f20f8166b5e9e042e81a0
Guilherme <Fireguiquenbr@gmail.com>
2023-10-01 13:34:12 -0300

    update

---
+++ Week 02 - Array/exercises/Limited-CalcAvarage.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    float scores[] = {5, 4, 8, 7};
+    int scoresLength = sizeof (scores) / sizeof (scores[0]);
+    float totalScore; 
+
+    for (int i = 0; scoresLength > i; i++)
+    {
+        totalScore += scores[i];
+    }
+
+    printf("Array length: %i\n", scoresLength);
+    printf("Total Score: %f\n", totalScore);
+    printf("Avarage: %f\n", totalScore / scoresLength);
+
+
+}
